6ce408b598cf0d73618905e025b484680bab4ef0
SalmanDeveloper490 <sk36663@gmail.com>
2020-05-01 13:44:56 +0530

    Updates

---
+++ precache-manifest.ceb1ce03ce00850bb71d614d872c8c07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354045aa576e30d883aa1bf0ef491343",
+    "url": "/react-covid19-webapp/index.html"
+  },
+  {
+    "revision": "a0090f38bc72978d03a9",
+    "url": "/react-covid19-webapp/static/css/main.f122c85e.chunk.css"
+  },
+  {
+    "revision": "25679049edcaf0608fc7",
+    "url": "/react-covid19-webapp/static/js/2.350618e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-covid19-webapp/static/js/2.350618e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0090f38bc72978d03a9",
+    "url": "/react-covid19-webapp/static/js/main.aba56570.chunk.js"
+  },
+  {
+    "revision": "904dd555f2c4b4d6bb52",
+    "url": "/react-covid19-webapp/static/js/runtime-main.893eef95.js"
+  },
+  {
+    "revision": "7541c305a1fd2a65f3f5bf674db6789a",
+    "url": "/react-covid19-webapp/static/media/slider-2.7541c305.jpg"
+  },
+  {
+    "revision": "074cb26015ec5b14aceb4cd5ad2372f7",
+    "url": "/react-covid19-webapp/static/media/slider-3.074cb260.jpg"
+  },
+  {
+    "revision": "1b2e066f5bac1cab1c21c4c648641e0b",
+    "url": "/react-covid19-webapp/static/media/slider-one.1b2e066f.jpg"
+  }
+]);
