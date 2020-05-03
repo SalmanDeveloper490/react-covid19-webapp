95d6e362315babfffc6706a7f02da64a9524fca0
SalmanDeveloper490 <sk36663@gmail.com>
2020-05-03 13:50:33 +0530

    Adding search to covid grid

--- src/Components/Coviddata.js
+++ src/Components/Coviddata.js
@@ -4,30 +4,55 @@ class Coviddata extends Component {
   constructor(){
       super();
       this.state={
-          countries:[]
+          countries:[],
+          searchData:null
       }
   }
   
-  componentDidMount() {
+  fetchData = () => {
+    fetch(`https://api.covid19api.com/summary`)
+    .then(actualdata => actualdata.json())
+    .then(realdata => {
+      const { Countries } = realdata;
+        this.setState({
+          allCountries: Countries,
+          countries: Countries
+        });
+    })
+    .catch(error => console.log(error));
+  }
 
-    fetch('https://api.covid19api.com/summary')
-      .then(actualdata => actualdata.json())
-      .then(realdata => {
-        console.log(realdata);
-        const { Countries } = realdata;
-          this.setState({
-            countries: Countries
-          })
-      })
-      .catch(error => console.log(error));
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  searchByCountryName(search) {
+    let filteredCountries = [];
+    if (search === '') {
+      filteredCountries = this.state.allCountries.slice();
+    } else {
+      const countries = this.state.allCountries.slice();
+      const countryName = search.toLowerCase();
+      filteredCountries = countries.filter(country => {
+        const searchRegex = new RegExp(countryName, 'i');
+        const countryN = country.Country.toLowerCase();
+        const countryCode = country.CountryCode.toLowerCase();
+        return countryN === countryName || countryCode === countryName || countryN.search(searchRegex) !== -1 || countryCode.search(searchRegex) !== -1
+      });
+    }
+
+    this.setState({ countries: filteredCountries });
   }
 
   render() {
     return (
       <Fragment>
         <div className="container pt-5">
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Search Country" onChange={(event) => this.searchByCountryName(event.target.value)} />
+          </div>
           <table className="table border shadow">
-            <thead class="bg-dark text-white">
+            <thead className="bg-dark text-white">
               <tr>
                 <th>Countries</th>
                 <th>Date</th>
@@ -42,7 +67,7 @@ class Coviddata extends Component {
             <tbody> 
                {
                this.state.countries.length ? this.state.countries.map(country => (
-                  <tr>
+                  <tr key={country.CountryCode}>
                       <td>{country.Country}</td>
                       <td>{new Date(country.Date).toDateString()}</td>
                       <td>{country.NewConfirmed}</td>
@@ -53,7 +78,7 @@ class Coviddata extends Component {
                       <td>{country.TotalRecovered}</td>
                   </tr>
                 )) : 
-                <h1 className="text-center">LOADING...</h1>
+                <tr><td>Loading...</td></tr>
                }
             </tbody>
           </table>
